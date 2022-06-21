1429b8bf8c0fabd167b4e51e86b1e1c0043deca7
Finn O'Prey <finn@oprey.co>
2022-06-21 12:19:01 +1000

    Created events cards for dashboard

--- dashboard.php
+++ dashboard.php
@@ -6,6 +6,8 @@
     <title>Pavilion Events</title>
     <link rel="stylesheet" href="assets/css/globals.css">
     <link rel="stylesheet" href="assets/css/dashboard.css">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 <body>
 <div class="container">
@@ -16,12 +18,134 @@
     <div class="events">
         <h2 class="events-row-heading heading-thisweek">This Week</h2>
         <div class="events-row">
-            <div class="event box-outlined"></div>
-            <div class="event box-outlined"></div>
-            <div class="event box-outlined"></div>
-            <div class="event box-outlined"></div>
-            <div class="event box-outlined"></div>
-            <div class="event box-outlined"></div>
+            <div class="event box-outlined">
+                <div class="flex-column">
+                    <img src="/assets/img/gym.png" class="event-image">
+                    <div class="flex-row">
+                        <div>
+                            <h3>Gym Session</h3>
+                            <h4>
+                                Come down to the gym and work your stress off. 
+                                We’ll be doing a circuit and heaps of activities!
+                            </h4>
+                        </div>
+                        <div class="event-details">
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">schedule</span>
+                                <p>4:00pm</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">event</span>
+                                <p>Saturday, 31st of May</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">near_me</span>
+                                <p>The Gym</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">group</span>
+                                <p>Joe, Kathy & Tim</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="event box-outlined">
+                <div class="flex-column">
+                    <img src="/assets/img/fun_run.png" class="event-image">
+                    <div class="flex-row">
+                        <div>
+                            <h3>Fun Run</h3>
+                            <h4>
+                                Just take a stroll, or compete to be the best. 
+                                It’s fun for everyone, this week at the fun run.
+                            </h4>
+                        </div>
+                        <div class="event-details">
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">schedule</span>
+                                <p>7:00pm</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">event</span>
+                                <p>Monday, 2nd of June</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">near_me</span>
+                                <p>Bury Park</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">group</span>
+                                <p>Markus & Jospeh</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="event box-outlined">
+                <div class="flex-column">
+                    <img src="/assets/img/basketball.png" class="event-image">
+                    <div class="flex-row">
+                        <div>
+                            <h3>Basketball</h3>
+                            <h4>
+                                Enjoy a basketball game for all skill levels, 
+                                for the sake of enjoyment and a good time.
+                            </h4>
+                        </div>
+                        <div class="event-details">
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">schedule</span>
+                                <p>7:00pm</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">event</span>
+                                <p>Monday, 2nd of June</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">near_me</span>
+                                <p>Bury Park</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">group</span>
+                                <p>Markus & Jospeh</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="event box-outlined">
+                <div class="flex-column">
+                    <img src="/assets/img/gym.png" class="event-image">
+                    <div class="flex-row">
+                        <div>
+                            <h3>Cool Things!</h3>
+                            <h4>
+                                Enjoy a basketball game for all skill levels, 
+                                for the sake of enjoyment and a good time.
+                            </h4>
+                        </div>
+                        <div class="event-details">
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">schedule</span>
+                                <p>7:00pm</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">event</span>
+                                <p>Monday, 2nd of June</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">near_me</span>
+                                <p>Bury Park</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">group</span>
+                                <p>Markus & Jospeh</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <h2 class="events-row-heading heading-upcoming">Upcoming</h2>
         <div class="events-row">
