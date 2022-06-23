8703ff8bb9df5e5694f97dac97acb765f3c93f29
Finn O'Prey <finn@oprey.co>
2022-06-23 11:31:33 +1000

    Finished event examples, improved readability

--- dashboard.php
+++ dashboard.php
@@ -22,9 +22,9 @@
                 <div class="flex-column">
                     <img src="/assets/img/gym.png" class="event-image">
                     <div class="flex-row">
-                        <div>
-                            <h3>Gym Session</h3>
-                            <h4>
+                        <div class="event-content-left">
+                            <h3 class="event-title">Gym Session</h3>
+                            <h4 class="event-description">
                                 Come down to the gym and work your stress off. 
                                 We’ll be doing a circuit and heaps of activities!
                             </h4>
@@ -54,9 +54,9 @@
                 <div class="flex-column">
                     <img src="/assets/img/fun_run.png" class="event-image">
                     <div class="flex-row">
-                        <div>
-                            <h3>Fun Run</h3>
-                            <h4>
+                        <div class="event-content-left">
+                            <h3 class="event-title">Fun Run</h3>
+                            <h4 class="event-description">
                                 Just take a stroll, or compete to be the best. 
                                 It’s fun for everyone, this week at the fun run.
                             </h4>
@@ -86,41 +86,9 @@
                 <div class="flex-column">
                     <img src="/assets/img/basketball.png" class="event-image">
                     <div class="flex-row">
-                        <div>
-                            <h3>Basketball</h3>
-                            <h4>
-                                Enjoy a basketball game for all skill levels, 
-                                for the sake of enjoyment and a good time.
-                            </h4>
-                        </div>
-                        <div class="event-details">
-                            <div class="event-detail">
-                                <span class="material-symbols-outlined">schedule</span>
-                                <p>7:00pm</p>
-                            </div>
-                            <div class="event-detail">
-                                <span class="material-symbols-outlined">event</span>
-                                <p>Monday, 2nd of June</p>
-                            </div>
-                            <div class="event-detail">
-                                <span class="material-symbols-outlined">near_me</span>
-                                <p>Bury Park</p>
-                            </div>
-                            <div class="event-detail">
-                                <span class="material-symbols-outlined">group</span>
-                                <p>Markus & Jospeh</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="event box-outlined">
-                <div class="flex-column">
-                    <img src="/assets/img/gym.png" class="event-image">
-                    <div class="flex-row">
-                        <div>
-                            <h3>Cool Things!</h3>
-                            <h4>
+                        <div class="event-content-left">
+                            <h3 class="event-title">Basketball</h3>
+                            <h4 class="event-description">
                                 Enjoy a basketball game for all skill levels, 
                                 for the sake of enjoyment and a good time.
                             </h4>
@@ -148,13 +116,105 @@
             </div>
         </div>
         <h2 class="events-row-heading heading-upcoming">Upcoming</h2>
-        <div class="events-row">
-            <div class="event box-outlined"></div>
-            <div class="event box-outlined"></div>
-            <div class="event box-outlined"></div>
-            <div class="event box-outlined"></div>
-            <div class="event box-outlined"></div>
-            <div class="event box-outlined"></div>
+        <div class="events-row upcoming-row">
+            <div class="event box-outlined">
+                <div class="flex-column">
+                    <img src="/assets/img/meditation.png" class="event-image filter-grayscale">
+                    <div class="flex-row">
+                        <div class="event-content-left">
+                            <h3 class="event-title">Meditation</h3>
+                            <h4 class="event-description">
+                                Let it all go for a while with some mindfulness.
+                                For all ages and fitness levels, wellness is for everyone!
+                            </h4>
+                        </div>
+                        <div class="event-details">
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">schedule</span>
+                                <p>9:00pm</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">event</span>
+                                <p>Monday, 22nd of August</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">near_me</span>
+                                <p>St Kilda Beach</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">group</span>
+                                <p>Tom, Markus & Ryan</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="event box-outlined">
+                <div class="flex-column">
+                    <img src="/assets/img/indigenous.png" class="event-image filter-grayscale">
+                    <div class="flex-row">
+                        <div class="event-content-left">
+                            <h3 class="event-title">Indigenous Art</h3>
+                            <h4 class="event-description">
+                                Work on some indigenous art in this weekly activity.
+                                Learn about indigenous and aboriginal history, from 
+                                the first nations people themselves.
+                            </h4>
+                        </div>
+                        <div class="event-details">
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">schedule</span>
+                                <p>9:00pm</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">event</span>
+                                <p>Monday, 22nd of August</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">near_me</span>
+                                <p>St Kilda Beach</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">group</span>
+                                <p>Tom, Markus & Ryan</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="event box-outlined">
+                <div class="flex-column">
+                    <img src="/assets/img/paint.png" class="event-image filter-grayscale">
+                    <div class="flex-row">
+                        <div class="event-content-left">
+                            <h3 class="event-title">Painting</h3>
+                            <h4 class="event-description">
+                                Never painted before? Or a seasoned expert? 
+                                Well regardless, don't fret! Come down and 
+                                have a blast painting with our expert team.
+                            </h4>
+                        </div>
+                        <div class="event-details">
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">schedule</span>
+                                <p>9:00pm</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">event</span>
+                                <p>Monday, 22nd of August</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">near_me</span>
+                                <p>St Kilda Beach</p>
+                            </div>
+                            <div class="event-detail">
+                                <span class="material-symbols-outlined">group</span>
+                                <p>Tom, Markus & Ryan</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
