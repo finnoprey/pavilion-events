92674f209d79268b7d15b896c079b6caec3056b9
Finn O'Prey <finn@oprey.co>
2022-07-28 12:06:51 +1000

    Created event modification ui

--- organizer.php
+++ organizer.php
@@ -15,12 +15,18 @@
     $account = unserialize($_SESSION['account']);
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-      $conn = mysqli_connect($config->db_address, $config->db_user, $config->db_password, $config->db_schema);
-      if (!$conn) {
-          $_SESSION['error_message'] = 'Could not connect to database. Please notify administrators.';
-          redirect('/login.php');
-          exit();
-      }
+        $conn = mysqli_connect($config->db_address, $config->db_user, $config->db_password, $config->db_schema);
+        if (!$conn) {
+            $_SESSION['error_message'] = 'Could not connect to database. Please notify administrators.';
+            redirect('/login.php');
+            exit();
+        }
+
+        if (isset($_POST['delete'])) {
+            exit('Delete');
+        } elseif (isset($_POST['submit'])) {
+            exit('Create');
+        }
     }
 ?>
 
@@ -81,29 +87,50 @@
           </div>
         </div>
         <div class="modify-section flex-column">
-          <h1 class="heading">Create Or Edit</h1>
-          <div class="box-outlined">
-            <form>
-              <label for="name">Name</label>
-              <input name="name" type="text">
-              <br>
-              <label for="description">Description</label>
-              <input name="description" type="text">
-              <br>
-              <label for="organizers">Organizers</label>
-              <input name="organizers" type="text">
-              <br>
-              <label for="place">Place</label>
-              <input name="place" type="text">
-              <br>
-              <label for="time">Time</label>
-              <input name="time" type="time">
-              <br>
-              <label for="date">Date</label>
-              <input name="date" type="date">
-              <br>
-              <input type="button" name="submit" value="Submit">
-              <input type="button" name="delete" value="Delete">
+        <h1 class="heading">Add</h1>
+          <div class="box-outlined box-form box-create">
+            <form action="organizer.php" method="POST">
+              <div class="flex-column">
+                <label for="name">Name</label>
+                <input name="name" type="text">
+              </div>
+              <div class="flex-column">
+                <label for="description">Description</label>
+                <textarea cols="20" rows="5" name="description" class="description-input"></textarea>
+              </div>
+              <div class="flex-row">
+                <div class="flex-column">
+                  <label for="organizers">Organizers</label>
+                  <input name="organizers" type="text" style="width: 120px; margin-right: 10px;">
+                </div>
+                <div class="flex-column">
+                  <label for="place">Place</label>
+                  <input name="place" type="text" style="width: 130px;">
+                </div>
+              </div>
+              <div class="flex-row">
+                <div class="flex-column">
+                  <label for="time">Time</label>
+                  <input name="time" type="time" style="width: 100px; margin-right: 10px;">
+                </div>
+                <div class="flex-column">
+                  <label for="date">Date</label>
+                  <input name="date" type="date" style="width: 120px;">
+                </div>
+              </div>
+              <input type="submit" name="submit" value="Create">
+            </form>
+          </div>
+          <h2 class="subheading">Delete</h2>
+          <div class="box-outlined box-form box-delete">
+            <form action="organizer.php" method="POST">
+              <div class="flex-row flex-centered">
+                <div class="flex-column"> 
+                  <label for="name">Name</label>
+                  <input name="name" type="text" style="width: 200px;">
+                </div>
+                <input type="submit" name="delete" value="Delete">
+              </div>
             </form>
           </div>
         </div>
